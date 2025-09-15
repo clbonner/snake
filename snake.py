7dc4d910e9081cff7aaa04a15a1ca86805de10fc
Chris Bonner <clawrence.bonner@gmail.com>
2025-09-15 21:52:34 +0100

    Fixed bug with snake position after game over.

--- snake.py
+++ snake.py
@@ -23,13 +23,22 @@ class Snake():
         self.posX = int(self.LCD.width / 2)
         self.posY = int(self.LCD.height / 2)
         self.size = 1
-        self.direction = 'E'
+        self.direction = 'N'
 
         # games attributes
         self.level = 1
         self.MAX_LEVEL = 5
         self.fruit = 5
         self.gameNotOver = True
+
+        # levels are defined by speed (ms)
+        self.LEVEL1 = 250
+        self.LEVEL2 = 200
+        self.LEVEL3 = 150
+        self.LEVEL4 = 100
+        self.LEVEL5 = 50
+
+        self.currentLevel = self.LEVEL1
 
         self.welcomeScreen()
 
@@ -55,15 +64,14 @@ class Snake():
     def checkForKeypress(self):
         if (self.up.value() == 0):
             self.direction = 'N'
+        if (self.right.value() == 0):
+            self.direction = 'E'
         if (self.down.value() == 0):
             self.direction = 'S'
         if (self.left.value() == 0):
             self.direction = 'W'
-        if (self.right.value() == 0):
-            self.direction = 'E'
 
     def checkOutOfBounds(self):
-        print("x ", self.posX, " y ", self.posY)
         if (self.posY <  2 or
             self.posX > self.LCD.width - 10 or
             self.posY > self.LCD.height - 10 or
@@ -72,9 +80,24 @@ class Snake():
 
     def gameOver(self):
         self.LCD.text("GAME OVER", int(self.LCD.width / 4.5), int(self.LCD.height / 2), self.LCD.RED)
-        self.gameNotOver = False
         self.LCD.show()
-        exit(0)
+
+        # reset snake position
+        self.posX = int(self.LCD.width / 2)
+        self.posY = int(self.LCD.height / 2)
+        self.direction = 'N'
+
+        self.waitForKeyPress()
+        self.welcomeScreen()
+        
+    def waitForKeyPress(self):
+        while (True):
+            time.sleep_ms(100)
+            if (self.left.value() == 0 or
+                self.right.value() == 0 or
+                self.up.value() == 0 or
+                self.down.value() == 0):
+                return
 
     def start(self):
         self.LCD.fill(self.LCD.BLACK)
@@ -82,8 +105,8 @@ class Snake():
         self.drawSnake()
         self.LCD.show()
 
-        while (self.gameNotOver):
-            for x in range(250):
+        while (True):
+            for x in range(self.currentLevel):
                 self.checkForKeypress()
                 time.sleep_ms(1)
             self.clearSnake()
@@ -92,27 +115,21 @@ class Snake():
             self.LCD.show()
             
     def setBorder(self):
-        self.LCD.hline(0,0,128,self.LCD.GBLUE)
-        self.LCD.hline(0,127,128,self.LCD.GBLUE)
-        self.LCD.vline(0,0,128,self.LCD.GBLUE)
-        self.LCD.vline(127,0,128,self.LCD.GBLUE)
+        self.LCD.hline(0, 0, 128, self.LCD.GBLUE)
+        self.LCD.hline(0, 127, 128, self.LCD.GBLUE)
+        self.LCD.vline(0, 0, 128, self.LCD.GBLUE)
+        self.LCD.vline(127, 0, 128, self.LCD.GBLUE)
 
     def welcomeScreen(self):
+        self.LCD.fill(self.LCD.BLACK)
         self.setBorder()
         self.LCD.text("SNAKE", 45, 25, self.LCD.GREEN)
         self.LCD.text("Press any key", 10, 50, self.LCD.BLUE)
         self.LCD.text("to start...", 20, 70, self.LCD.BLUE)
-
         self.LCD.show()
 
-        while (True):
-            if (self.left.value() == 0 or
-                self.right.value() == 0 or
-                self.up.value() == 0 or
-                self.down.value() == 0):
-                    self.start()
-
-
+        self.waitForKeyPress()
+        self.start()
 
 if __name__=='__main__':
     game = Snake()
